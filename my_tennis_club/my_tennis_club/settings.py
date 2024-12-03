8e587148a6b7b6f6cd499e9606166c41d2179451
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-12-03 17:43:39 +0800

    feat: created super user

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o33ii1wa3j)(5^cq9*$ixh6t$s#u^x9!o)xl4y6ttif=_zl=3u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
