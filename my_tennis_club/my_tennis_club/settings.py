6ee89c12764230fa20b8318e9f7200a8f6bcfdd7
Pearl Khin <pearlkhin@Pearls-Mac-mini.local>
2024-12-06 21:08:54 +0800

    feat: connect AWS RDS and add config ebextensions

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'masteruser',
+        'PASSWORD': 'Master123!',
+        'HOST': 'w3-django-project.cva6eyq085bl.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '5432'
     }
 }
 
